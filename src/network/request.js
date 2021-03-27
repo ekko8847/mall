6e79a5687b44be376c829b805c07253c1e8ce7c4
ekko8847 <867635716@qq.com>
2021-03-27 22:44:26 +0800

    style: 删除多余代码

--- src/network/request.js
+++ src/network/request.js
@@ -1,23 +1,23 @@
 import axios from 'axios'
 
 
-export function request(config){
-  const instance = axios.create({
-    baseURL:'http://152.136.185.210:8000/api/w6',
-    timeout:5000
-  })
+export function request(config) {
+    const instance = axios.create({
+        baseURL: '接口找老师要',
+        timeout: 5000
+    })
 
-  instance.interceptors.request.use(config=>{
-    return config
-  },error => {
-    console.log(err)
-  })
+    instance.interceptors.request.use(config => {
+        return config
+    }, error => {
+        console.log(err)
+    })
 
-  instance.interceptors.response.use(res=>{
-    return res.data
-  },err=>{
-    console.log(err)
-  })
+    instance.interceptors.response.use(res => {
+        return res.data
+    }, err => {
+        console.log(err)
+    })
 
-  return instance(config)
+    return instance(config)
 }
